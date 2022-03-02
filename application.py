f0d438db7b20bda6020eeff323319b757c361b8d
jasmeet.singh <jasmeet.singh@softobiz.com>
2022-03-02 12:28:46 +0530

    cors policy and cross_origin setup

--- application.py
+++ application.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from Home import h_blueprint
 from Signup import signup_blueprint
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from Saved_stories import saved_blueprint
 
 application = Flask(__name__)
-CORS(application, resources={
-    f'/*'
-})
+cors = CORS(application)
+application.config['CORS_HEADERS'] = 'Content-Type'
 
 @application.route("/")
+@cross_origin()
 def starting_url():
 	return "Homepage to the News Aggregator Api", 200
 
